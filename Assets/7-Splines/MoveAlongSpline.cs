1e9aedb6d408cba6e116d4b5e1fdf5b8adac7cd8
Mahmoud Mohamed <44034267+shimos12468@users.noreply.github.com>
2024-08-12 14:35:08 +0300

    kjhj

--- Assets/7-Splines/MoveAlongSpline.cs
+++ Assets/7-Splines/MoveAlongSpline.cs
@@ -8,19 +8,21 @@ public class MoveAlongSpline : MonoBehaviour
 {
     public SplineContainer splinesContainer;
     public float speed = 1f;
-    float distancePercentage = 0f;
-    float splineLength;
-    int index = 1;
-    int splineIndex = 1;
-    public Transform spline2Start;
 
-    // Start is called before the first frame update
+    public List<GameObject> woodenPlates = new List<GameObject>();
+    public List<float>splineLength;
+    public List<int> splineIndex;
+    public List<float> distancePercentages;
+
+    public GameObject prefab;
+
+    int index = 1;
+    
+    
     void Start()
     {
-        splineLength = splinesContainer.CalculateLength(splineIndex);
-
-
-
+        PopulatePlates();
+        //InitializePlates();
 
     }
 
@@ -28,53 +30,131 @@ public class MoveAlongSpline : MonoBehaviour
     void Update()
     {
 
-
-       
-        distancePercentage += speed * Time.deltaTime / splineLength;
-        Vector3 currentPosition = splinesContainer.EvaluatePosition(splineIndex, distancePercentage);
-        transform.position = currentPosition;
-        if (Vector3.Distance(transform.position, spline2Start.position) <= 0.01f && splineIndex == 0)
-        {
-            print("hey ? ");
-            splineIndex = 1;
-            splineLength = splinesContainer.CalculateLength(splineIndex);
-            distancePercentage = 0;
-
-        }
-        else if (distancePercentage > 1f)
+        
+        for(int i = 0; i < woodenPlates.Count; i++)
         {
 
-            //distancePercentage = 0;
-            float min = Mathf.Infinity;
-            for (int i = 0; i < splinesContainer.Splines[0].Knots.ToArray().Length; i++)
+
+            
+
+            /*distancePercentages[i] += speed * Time.deltaTime / splineLength[i];
+
+
+
+
+            Vector3 currentPosition = splinesContainer.EvaluatePosition(splineIndex[i], distancePercentages[i]);
+
+            woodenPlates[i].transform.position = currentPosition;
+
+            if (Vector3.Distance(woodenPlates[i].transform.position, splinesContainer.Splines[1].Knots.ToArray()[0].Position) <= 0.01f && splineIndex[i] == 0)
             {
-
-                float newmin = Mathf.Min(Vector3.Distance(splinesContainer.Splines[0].Knots.ToArray()[i].Position, splinesContainer.Splines[1].Knots.ToArray()[splinesContainer.Splines[1].Knots.ToArray().Length - 1].Position), min);
-
-                if (newmin < min)
-                {
-                    min = newmin;
-                    index = i;
-                }
-
+                print("hey ? ");
+                splineIndex[i] = 1;
+                splineLength[i] = splinesContainer.CalculateLength(splineIndex[i]);
+                distancePercentages[i] = 0;
 
             }
-            splineIndex = 0;
-            splineLength = splinesContainer.CalculateLength(splineIndex);
-            distancePercentage = Vector3.Distance(splinesContainer.Splines[0].Knots.ToArray()[0].Position, splinesContainer.Splines[0].Knots.ToArray()[index].Position) / splineLength;
+            else if (distancePercentages[i] > 1f)
+            {
 
-            Vector3 nextPosition = splinesContainer.Splines[0].Knots.ToArray()[index].Position;
-            Vector3 direction = nextPosition - currentPosition;
-            transform.rotation = Quaternion.LookRotation(direction, transform.up);
-        }
-        else
-        {
-            Vector3 nextPosition = splinesContainer.EvaluatePosition(splineIndex, distancePercentage + 0.05f);
-            Vector3 direction = nextPosition - currentPosition;
-            transform.rotation = Quaternion.LookRotation(direction, transform.up);
+                //distancePercentage = 0;
+                float min = Mathf.Infinity;
+                for (int k = 0; k < splinesContainer.Splines[0].Knots.ToArray().Length; k++)
+                {
+
+                    float newmin = Mathf.Min(Vector3.Distance(splinesContainer.Splines[0].Knots.ToArray()[k].Position, splinesContainer.Splines[1].Knots.ToArray()[splinesContainer.Splines[1].Knots.ToArray().Length - 1].Position), min);
+
+                    if (newmin < min)
+                    {
+                        min = newmin;
+                        index = k;
+                    }
+
+
+                }
+                splineIndex[i] = 0;
+                splineLength[i] = splinesContainer.CalculateLength(splineIndex[i]);
+                distancePercentages[i] = Vector3.Distance(splinesContainer.Splines[0].Knots.ToArray()[0].Position, splinesContainer.Splines[0].Knots.ToArray()[index].Position * splinesContainer.gameObject.transform.localScale) / splineLength[i];
+
+                Vector3 nextPosition = splinesContainer.Splines[0].Knots.ToArray()[index].Position;
+                Vector3 direction = nextPosition - currentPosition;
+                woodenPlates[i].transform.rotation = Quaternion.LookRotation(direction, transform.up);
+            }
+            else
+            {
+                Vector3 nextPosition = splinesContainer.EvaluatePosition(splineIndex[i], distancePercentages[i] + 0.05f);
+                Vector3 direction = nextPosition - currentPosition;
+                woodenPlates[i].transform.rotation = Quaternion.LookRotation(direction, transform.up);
+
+            }*/
 
         }
 
 
     }
+
+
+    public int initialAngle = 45;
+
+    int numberOfObjects = 10;
+    float radius = 0.5f;
+    float mm = Mathf.Infinity;
+    int ind;
+    public void PopulatePlates()
+    {
+        for(int i = 0; i < numberOfObjects; i++)
+        {
+            var angle = i * Mathf.PI * 2 / numberOfObjects;
+            var pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+
+            GameObject go = Instantiate(prefab,pos,Quaternion.identity);
+
+
+
+            for(int j = 0;j< splinesContainer.Splines[0].Knots.ToArray().Length; j++)
+            {
+                float min= Vector3.Distance(go.transform.position, splinesContainer.Splines[0].Knots.ToArray()[j].Position);
+                if (min <= mm)
+                {
+                    mm = min;   
+                    ind = j;
+                }
+            
+            
+            }
+            print(ind);
+            go.transform.position = splinesContainer.Splines[0].Knots.ToArray()[ind].Position;
+            woodenPlates.Add(go);
+            mm = Mathf.Infinity;
+
+        }
+
+
+        InitializePlates();
+    }
+
+
+    void InitializePlates()
+    {
+        splineLength.Capacity = woodenPlates.Count;
+        splineIndex.Capacity = woodenPlates.Count;
+        distancePercentages.Capacity = woodenPlates.Count;
+
+        for (int i = 0; i < woodenPlates.Count; i++)
+        {
+            distancePercentages.Add(0);
+        }
+
+
+        for (int i = 0; i < woodenPlates.Count; i++)
+        {
+            splineIndex.Add(0);
+        }
+
+        for (int i = 0; i < woodenPlates.Count; i++)
+        {
+            splineLength.Add(splinesContainer.CalculateLength(splineIndex[i]));
+        }
+    }
+
 }
